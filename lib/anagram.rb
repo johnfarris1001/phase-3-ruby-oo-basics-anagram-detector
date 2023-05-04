d719a9d68f51f21846355c9244e46717b8533f42
John <johnfarris217@gmail.com>
2023-05-03 22:36:44 -0500

    commit tree 3f2595e4bb47426b48d78facaf40c2b7d375f3f6

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match array
+        return_array = []
+        array.each do |w|
+            if w.chars.sort == @word.chars.sort
+                return_array.push(w)
+            else
+            end
+        end
+        return_array
+    end
+
+end
